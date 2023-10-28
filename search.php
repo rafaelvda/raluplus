0df67081124167f15a08342387021ab5f41e1cf9
Rafael <rafaelvmico@gmail.com>
2023-10-28 15:37:54 +0200

    adding search bar

--- search.php
+++ search.php
@@ -16,6 +16,13 @@
 
     <h1>Rechercher</h1>
 
+    <div class="container mt-3">
+        <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Titre, film ou série" aria-label="Rechercher">
+            <button class="btn btn-outline-light" type="submit">Rechercher</button>
+        </form>
+    </div>
+
     <?php require 'commun/footer.html'?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
