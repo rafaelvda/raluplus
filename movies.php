bb92711e53b1c07eabacfa4aa2e67a259bc02be9
Rafael <rafaelvmico@gmail.com>
2023-10-28 16:19:29 +0200

    adding card samples

--- movies.php
+++ movies.php
@@ -16,6 +16,75 @@
 
     <h1>Films</h1>
 
+    <Section id="list">
+    <div class="row mb-3 text-leading">
+        <div class="col-md-3 themed-grid-col">
+            <div class="card text-bg-dark" style="width:250px;" onclick="redirectToPage('details.php')">
+                <img src="assets/cat/starwars.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h6 class="card-title">Card title</h6>
+                    </div>
+                </div>
+        </div>
+        <div class="col-md-3 themed-grid-col">
+            <div class="card text-bg-dark" style="width:250px;" onclick="redirectToPage('details.php')">
+                <img src="assets/cat/starwars.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h6 class="card-title">Card title</h6>
+                    </div>
+                </div>
+        </div>
+        <div class="col-md-3 themed-grid-col">
+            <div class="card text-bg-dark" style="width:250px;" onclick="redirectToPage('details.php')">
+                <img src="assets/cat/starwars.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h6 class="card-title">Card title</h6>
+                    </div>
+                </div>
+        </div>
+        <div class="col-md-3 themed-grid-col">
+            <div class="card text-bg-dark" style="width:250px;" onclick="redirectToPage('details.php')">
+                <img src="assets/cat/starwars.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h6 class="card-title">Card title</h6>
+                    </div>
+                </div>
+        </div>
+        <div class="col-md-3 themed-grid-col">
+            <div class="card text-bg-dark" style="width:250px;" onclick="redirectToPage('details.php')">
+                <img src="assets/cat/starwars.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h6 class="card-title">Card title</h6>
+                    </div>
+                </div>
+        </div>
+        <div class="col-md-3 themed-grid-col">
+            <div class="card text-bg-dark" style="width:250px;" onclick="redirectToPage('details.php')">
+                <img src="assets/cat/starwars.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h6 class="card-title">Card title</h6>
+                    </div>
+                </div>
+        </div>
+        <div class="col-md-3 themed-grid-col">
+            <div class="card text-bg-dark" style="width:250px;" onclick="redirectToPage('details.php')">
+                <img src="assets/cat/starwars.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h6 class="card-title">Card title</h6>
+                    </div>
+                </div>
+        </div>
+        <div class="col-md-3 themed-grid-col">
+            <div class="card text-bg-dark" style="width:250px;" onclick="redirectToPage('details.php')">
+                <img src="assets/cat/starwars.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h6 class="card-title">Card title</h6>
+                    </div>
+                </div>
+        </div>
+    </div>
+   </Section>
+
     <?php require 'commun/footer.html'?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script></body>
